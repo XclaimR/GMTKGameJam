abbc6b0b95fdf4278543c3d99a152eba53261583
Pedro Poblete Lasserre <pedro@pobletelasserre.me>
2021-06-12 17:43:37 -0400

    Created boundaries for player so it cannot leave the play area

--- GMTK/Assets/Scripts/Actions/Move.cs
+++ GMTK/Assets/Scripts/Actions/Move.cs
@@ -13,12 +13,19 @@ public class Move : MonoBehaviour
     private Rigidbody2D playerRigidbody;
     private float horizontalMove = 0;
     private float verticalMove = 0;
-    private Vector2 mousePosition; 
+    private Vector2 mousePosition;
+
+    private float halfPlayerSizeX;
+    private float halfPlayerSizeY;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+
+        halfPlayerSizeX = GetComponent<BoxCollider2D>().size.x / 3;
+        halfPlayerSizeY = GetComponent<BoxCollider2D>().size.y / 3;
+
     }
 
     // Update is called once per frame
@@ -32,8 +39,25 @@ public class Move : MonoBehaviour
         animatortop.SetFloat("Speed", Mathf.Max(Mathf.Abs(horizontalMove),Mathf.Abs(verticalMove)));
         animatorbottom.SetFloat("Speed", Mathf.Max(Mathf.Abs(horizontalMove), Mathf.Abs(verticalMove)));
 
-        Debug.Log(Mathf.Max(Mathf.Abs(horizontalMove), Mathf.Abs(verticalMove)));
         mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        clampPlayerMovement();
+    }
+
+    private void clampPlayerMovement()
+    {
+        Vector3 position = transform.position;
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + halfPlayerSizeX;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - halfPlayerSizeX;
+        float topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).y + halfPlayerSizeY;
+        float bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distance)).y - halfPlayerSizeY;
+
+        position.x = Mathf.Clamp(position.x, leftBorder, rightBorder);
+        position.y = Mathf.Clamp(position.y, topBorder, bottomBorder);
+        transform.position = position;
     }
 
     private void FixedUpdate()
