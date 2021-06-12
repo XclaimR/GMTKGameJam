3c4d238d512c7a283ff0a3fc3f8a887269a51c3e
XclaimR <36238051+XclaimR@users.noreply.github.com>
2021-06-12 08:47:08 +0530

    Initial Commit

---
+++ GMTK/Assets/Scripts/Actions/Move.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0;
+
+    [SerializeField]
+    Camera mainCamera;
+
+    private Rigidbody2D playerRigidbody;
+    private float horizontalMove = 0;
+    private float verticalMove = 0;
+    private Vector2 mousePosition; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = new Vector2(horizontalMove * speed, verticalMove * speed);
+
+        Vector2 direction = mousePosition - playerRigidbody.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        playerRigidbody.rotation = angle;
+    }
+}
